3b50e549148380cc13711864dab2f1e0cc2d0529
putriulhaq0609@gmail.com <putriulhaq0609@gmail.com>
2022-12-08 15:14:41 +0700

    some code temporary comment

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const cors = require('cors');
 const { signup, login, post, like, comment, logout } = require('./routes');
 const connect = require("./models/index");
 
